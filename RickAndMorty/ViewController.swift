9b4b38343ad0f44b33027a100aee835623576922
Rodrigo Filomeno <rodrigo.filomeno@radixeng.com.br>
2023-01-08 09:26:04 -0300

    Inicio do Projeto

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Rodrigo Filomeno on 08/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
